bac71e171aa53c13c7ce65db0a02ba0898c3c508
Daniel Lobo <dajolo1995@gmail.com>
2022-11-09 12:59:48 -0500

    selections

--- src/components/data/contents.jsx
+++ src/components/data/contents.jsx
@@ -20,13 +20,9 @@ const contents = () => {
         `http://45.33.117.178:3050/meters/1057?begin_date=%222022-11-${day}T00:00:00.469Z%22&end_date=%222022-11-${day}T23:59:59.469Z%22%27`
       );
 
-      console.log(data);
-
       const datas = await axios.get(
         `http://45.33.117.178:3050/meters/1054?begin_date=%222022-11-${day}T00:00:00.469Z%22&end_date=%222022-11-${day}T23:59:59.469Z%22%27`
       );
-
-      console.log(datas);
 
       let totalEnergyConsumed = 0;
       let Tree = 0;
@@ -41,16 +37,12 @@ const contents = () => {
         co2 = co2 + e.data.calculation_co2;
       });
 
-      console.log(sums, "vmeter 1057");
-
       datas?.data.forEach((e) => {
         sums2 = sums2 + e.data.consumption;
         totalEnergyConsumed = totalEnergyConsumed + e.data.consumption;
         Tree = Tree + e.data.calculation_tree;
         co2 = co2 + e.data.calculation_co2;
       });
-
-      console.log(sums2, "vmeter 1054");
 
       let variable = {
         totalEnergyConsumed,
